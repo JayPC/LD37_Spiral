13f04deaefc29145bc4508e529b165ab9c2e287b
John Close <John@DESKTOP-J1TGLST.(none)>
2016-12-10 07:13:48 -0500

    Updates to the pickup script

---
+++ Scripts/AltFloorCounter.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum FloorNum{
+	FLOOR_ZERO, 
+	FLOOR_THREE, 
+	FLOOR_ONE, 
+	FLOOR_TWO, 
+	FLOOR_FOUR
+}
+
+
+public class AltFloorCounter : MonoBehaviour {
+	public int floorCount;
+	public bool positiveEnterDirection = true; //if true then the rotation was positive when entering; 
+	public bool plyaerTurnDirection = true; //if true then the rotation was positive when entering; 
+
+	public Rigidbody rigid;
+	public MeshRenderer renderer;
+	public Collider collider;
+	public FloorNum floorNum;
+	// Use this for initialization
+	void Start () {
+		rigid = this.GetComponent<Rigidbody>();
+		renderer = this.GetComponent<MeshRenderer>();
+		collider = this.GetComponent<Collider>();
+		switch (floorNum) 
+		{
+			case FloorNum.FLOOR_ZERO:
+			  floorCount = 0;
+			  break;
+			case FloorNum.FLOOR_THREE:
+			  floorCount = 1;
+			  break;
+			case FloorNum.FLOOR_ONE:
+			  floorCount = 2;
+			  break;
+			case FloorNum.FLOOR_FOUR:
+			  floorCount = 3;
+			  break;
+			case FloorNum.FLOOR_TWO:
+			  floorCount = 4;
+			  break;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(floorCount == 0){
+			renderer.enabled = true;
+			rigid.isKinematic = false;
+			collider.isTrigger = false;
+		} else {
+			renderer.enabled = false;
+			rigid.isKinematic = true;
+			collider.isTrigger = true;
+		}
+
+		if(floorCount == 5){
+			floorCount = 0;
+		} else if(floorCount == -1){
+			floorCount = 4;
+		}
+		switch (floorCount) 
+		{
+			case 0:
+			  floorNum = FloorNum.FLOOR_ZERO;
+			  break;
+			case 1:
+			  floorNum = FloorNum.FLOOR_THREE;
+			  break;
+			case 2:
+			  floorNum = FloorNum.FLOOR_ONE;
+			  break;
+			case 3:
+			  floorNum = FloorNum.FLOOR_FOUR;
+			  break;
+			case 4:
+			  floorNum = FloorNum.FLOOR_TWO;
+			  break;
+		}
+		plyaerTurnDirection = CharController.positiveRotation;
+	}
+
+	public void OnTriggerExit(Collider coll)
+	{
+		if(coll.tag == "SpinTrigger")
+		{
+			if(CharController.positiveRotation){
+				floorCount -= 1;
+			} else if(!CharController.positiveRotation){
+				floorCount += 1;
+			}
+		}
+	}
+
+	public void OnTriggerEnter(Collider coll)
+	{
+		if(coll.tag == "SpinTrigger")
+		{
+			if(CharController.positiveRotation){
+				floorCount -= 1;
+			} else if(!CharController.positiveRotation){
+				floorCount += 1;
+			}
+		}
+	}
+}
--- Scripts/ObjectInteraction.cs
+++ Scripts/ObjectInteraction.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PickupObject : MonoBehaviour {
+public class ObjectInteraction : MonoBehaviour {
 	public GameObject caryPoint; //These must be linked in the Editor
 	public Camera camera; //These must be linked in the Editor
 	public GameObject caryObject;
@@ -12,42 +12,65 @@ public class PickupObject : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButton(0)){
-			Debug.Log("LockingMouse");
-			Cursor.lockState = CursorLockMode.Locked;
-			Ray ray = new Ray (camera.transform.position, (camera.transform.position) *10);
-			RaycastHit hit;
 
+		//****************
+		//  Mouse Cursor
+		//****************
+		if(Input.GetMouseButtonDown(0)){
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+		
+		if (Input.GetKeyDown("escape")){
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+		//******************
+		//  Pickup Object
+		//******************
+		if(Input.GetMouseButton(0) && caryObject == null){
+			Cursor.lockState = CursorLockMode.Locked;
 			if(caryObject == null){
 				//If we're not carying anything and hitting something
-				if(Physics.Raycast (camera.transform.position, camera.transform.forward, out hit, 6)){
-					Debug.DrawLine(ray.origin, hit.point, Color.blue);
-					if(hit.transform.gameObject.tag == "PuzzlePiece"){
-						caryObject = hit.transform.gameObject;
+				var hits = Physics.RaycastAll (camera.transform.position, camera.transform.forward, 6);
+				if(hits.Length > 0){
+					for(var i=0;i<=hits.Length-1;i++){
+						if(hits[i].transform.tag=="PuzzlePiece"){
+							if(hits[i].transform.gameObject.GetComponent<MeshRenderer>() != null && hits[i].transform.gameObject.GetComponent<MeshRenderer>().enabled == true){
+								caryObject = hits[i].transform.gameObject;
+							}
+						}
 					}
 				}
-				if(Physics.Raycast (camera.transform.position, camera.transform.forward, out hit, 10)){
-					Debug.DrawLine(ray.origin, hit.point, Color.red);
-					if(hit.transform.gameObject.tag == "InteractObject"){
-						hit.transform.gameObject.SendMessage("Interact", SendMessageOptions.DontRequireReceiver);
-					}
-				}
-			}
-
-			if(caryObject != null){
-				caryObject.transform.position = caryPoint.transform.position;
+				
 			}
 		}
+
 		if(Input.GetMouseButtonUp(0)){
 			caryObject = null;
 		}
 
-
-		if (Input.GetKeyDown("escape")){
-			Cursor.lockState = CursorLockMode.None;
+		if(caryObject != null){
+			caryObject.transform.position = caryPoint.transform.position;
 		}
-	}
 
-	 void OnMouseDown() {
+
+
+
+		
+		//****************************
+		//  Interact With Object
+		//****************************
+		if(Input.GetMouseButtonDown(0)){
+			Ray ray = new Ray (camera.transform.position, (camera.transform.position) *10);
+			RaycastHit hit;
+
+			if(Physics.Raycast (camera.transform.position, camera.transform.forward, out hit, 10)){
+				Debug.DrawLine(ray.origin, hit.point, Color.red);
+				if(hit.transform.gameObject.tag == "InteractObject"){
+					hit.transform.gameObject.SendMessage("Interact", SendMessageOptions.DontRequireReceiver);
+				}
+			}
+		}
+		
 	}
 }
