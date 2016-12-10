788420fa22fe8b3aecb8766eb90e1869c9278038
John Close <John@DESKTOP-J1TGLST.(none)>
2016-12-10 01:18:09 -0500

    First Commit

---
+++ Scripts/CharController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharController : MonoBehaviour {
+	public static float rotateCount;
+	public static bool positiveRotation = false;
+	public float mouseSpeed;
+
+	public GameObject camera;
+	public Vector3 turn;
+	public Vector3 move;
+	public float movementSpeed = 10;
+	public float jumpPower = 50;
+	Rigidbody rigid;
+	float playerRotation = 0;
+	public float playerVerticalLookAngle = 60;
+
+	// Use this for initialization
+	void Start () {
+		rigid = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 movementAxis = new Vector3();
+		Vector3 turnAxis = new Vector3();
+		
+
+		if(Input.GetAxis("Horizontal") != 0){
+			movementAxis.x = Input.GetAxis("Horizontal");
+		}
+
+		if(Input.GetAxis("Vertical") != 0){
+			movementAxis.z = Input.GetAxis("Vertical");
+		}
+
+		if(Input.GetButtonDown("Jump")){
+			rigid.AddForce(new Vector3(0, jumpPower, 0));
+		}
+
+		rigid.AddRelativeForce(movementAxis.normalized * movementSpeed);
+
+		if(Input.GetAxis("Mouse X") != 0){
+			turnAxis.x = Input.GetAxis("Mouse X");
+			playerRotation += turnAxis.x;
+		}
+		//Debug.Log(playerRotation);
+		//Debug.Log(turnAxis.x);
+
+		if(Input.GetAxis("Mouse Y") != 0){
+			turnAxis.y = Input.GetAxis("Mouse Y");
+		}
+
+		this.transform.Rotate(new Vector3(0, turnAxis.x, 0));
+
+		this.camera.transform.Rotate(new Vector3(turnAxis.y, 0, 0));
+		turn = turnAxis;
+	}
+}
---
+++ Scripts/FloorCounter.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorCounter : MonoBehaviour {
+	public int floorCount;
+	public bool positiveEnterDirection = true; //if true then the rotation was positive when entering; 
+	public bool plyaerTurnDirection = true; //if true then the rotation was positive when entering; 
+
+	public Rigidbody rigid;
+	public MeshRenderer renderer;
+	public Collider collider;
+	// Use this for initialization
+	void Start () {
+		rigid = this.GetComponent<Rigidbody>();
+		renderer = this.GetComponent<MeshRenderer>();
+		collider = this.GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(floorCount == 0){
+			renderer.enabled = true;
+			rigid.isKinematic = false;
+			collider.isTrigger = false;
+		} else {
+			renderer.enabled = false;
+			rigid.isKinematic = true;
+			collider.isTrigger = true;
+		}
+
+		if(floorCount == 5){
+			floorCount = 0;
+		} else if(floorCount == -1){
+			floorCount = 4;
+		}
+
+		plyaerTurnDirection = CharController.positiveRotation;
+	}
+
+	public void OnTriggerExit(Collider coll)
+	{
+		if(coll.tag == "SpinTrigger")
+		{
+			if(positiveEnterDirection){
+				//entering and leaving from the same direction does something
+				if(CharController.positiveRotation){
+					floorCount += 1;
+				}
+			} else if(!positiveEnterDirection){
+				if(!CharController.positiveRotation){
+					floorCount -= 1;
+				}
+			}
+		}
+	}
+
+	public void OnTriggerEnter(Collider coll)
+	{
+		if(coll.tag == "SpinTrigger")
+		{	
+			if(CharController.positiveRotation){
+				positiveEnterDirection = true;
+			} else if(!CharController.positiveRotation){
+				positiveEnterDirection = false;
+			}
+		}
+	}
+}
