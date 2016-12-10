eeeacbbaf6bd2adde4307cd066c28943af8d1fde
John Close <John@DESKTOP-J1TGLST.(none)>
2016-12-10 13:00:08 -0500

    Working?

--- Scripts/Player/CharController.cs
+++ Scripts/Player/CharController.cs
@@ -16,6 +16,8 @@ public class CharController : MonoBehaviour {
 	float playerRotation = 0;
 	public float playerVerticalLookAngle = 60;
 
+	public bool blockInput = false;
+
 	// Use this for initialization
 	void Start () {
 		rigid = this.GetComponent<Rigidbody>();
@@ -23,43 +25,45 @@ public class CharController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 movementAxis = new Vector3();
-		Vector3 turnAxis = new Vector3();
-		
+		if(!blockInput){
+			Vector3 movementAxis = new Vector3();
+			Vector3 turnAxis = new Vector3();
+			
 
-		if(Input.GetAxis("Horizontal") != 0){
-			movementAxis.x = Input.GetAxis("Horizontal");
+			if(Input.GetAxis("Horizontal") != 0){
+				movementAxis.x = Input.GetAxis("Horizontal");
+			}
+
+			if(Input.GetAxis("Vertical") != 0){
+				movementAxis.z = Input.GetAxis("Vertical");
+			}
+
+			if(Input.GetButtonDown("Jump")){
+				rigid.AddRelativeForce(new Vector3(0, jumpPower, 0));
+			}
+
+			rigid.AddRelativeForce(movementAxis.normalized * movementSpeed);
+
+			if(Input.GetAxis("Mouse X") != 0){
+				turnAxis.x = Input.GetAxis("Mouse X");
+				playerRotation += turnAxis.x;
+			}
+			if(turnAxis.x > 0){
+				positiveRotation = true;
+			} else if(turnAxis.x < 0){
+				positiveRotation = false;
+			}
+			this.transform.Rotate(0, turnAxis.x, 0);
+
+			if(Input.GetAxis("Mouse Y") != 0){
+				turnAxis.y = Input.GetAxis("Mouse Y");
+			}
+			verticalLook += turnAxis.y;
+			verticalLook = Mathf.Clamp(verticalLook, -playerVerticalLookAngle, playerVerticalLookAngle);
+
+			float look = 360+verticalLook;
+			this.camera.transform.eulerAngles = this.transform.eulerAngles + new Vector3(look, 0, 0);
+			turn = turnAxis;
 		}
-
-		if(Input.GetAxis("Vertical") != 0){
-			movementAxis.z = Input.GetAxis("Vertical");
-		}
-
-		if(Input.GetButtonDown("Jump")){
-			rigid.AddRelativeForce(new Vector3(0, jumpPower, 0));
-		}
-
-		rigid.AddRelativeForce(movementAxis.normalized * movementSpeed);
-
-		if(Input.GetAxis("Mouse X") != 0){
-			turnAxis.x = Input.GetAxis("Mouse X");
-			playerRotation += turnAxis.x;
-		}
-		if(turnAxis.x > 0){
-			positiveRotation = true;
-		} else if(turnAxis.x < 0){
-			positiveRotation = false;
-		}
-		this.transform.Rotate(0, turnAxis.x, 0);
-
-		if(Input.GetAxis("Mouse Y") != 0){
-			turnAxis.y = Input.GetAxis("Mouse Y");
-		}
-		verticalLook += turnAxis.y;
-		verticalLook = Mathf.Clamp(verticalLook, -playerVerticalLookAngle, playerVerticalLookAngle);
-
-		float look = 360+verticalLook;
-		this.camera.transform.eulerAngles = this.transform.eulerAngles + new Vector3(look, 0, 0);
-		turn = turnAxis;
 	}
 }
--- Scripts/Player/IntroExplosion.cs
+++ Scripts/Player/IntroExplosion.cs
@@ -1,11 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class IntroExplosion : MonoBehaviour {
 	public Vector3 postExplosionSpawn;
+	public bool fadeOut = false;
+	public float fadeOutSpeed = 100;
+	public float fadeInSpeed = 1000;
+	public float blackAlpha = 0;
+	public bool fadeIn = false;
 
+	GameObject blackUIImage;
+	GameObject targetLookat;
+
+	public float playerKnockback = 0;
+	public void Update(){
+		if(fadeOut){
+			blackAlpha += Time.deltaTime  * fadeOutSpeed;
+			this.transform.rotation = Quaternion.AngleAxis(blackAlpha, Vector3.right);;
+		}
+
+		if(blackAlpha >= 255){
+			this.transform.rotation = Quaternion.identity;
+			this.transform.position = postExplosionSpawn;
+
+		}
+	}
 	public void StartExplosion(){
 		//Black out
-		this.transform.position = postExplosionSpawn;
+		fadeOut = true;
 	}
 }
